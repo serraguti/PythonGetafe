f2bbaa6a3174da618d1489edc4e6348e057766fe
serraguti <pacoserranox@gmail.com>
2025-02-07 19:33:22 +0100

    aaa

--- python03mates.py
+++ python03mates.py
@@ -1,13 +1,16 @@
 print("Ejemplo mates variables")
-numero1 = 20
-numero2 = 3
-suma = numero1 + numero2
+print("Introduzca el número 1")
+numero1 = int(input())
+print("Introduzca número 2")
+numero2 = int(input())
+suma = "numero1" + numero2
+print("Suma: ", suma)
 multiplicacion = numero1 * numero2
 division = numero1 / numero2
 resta = numero1 - numero2
 print("Resta ", resta)
 print(str(numero1) + " * " + str(numero2) + "=" + str(multiplicacion))
 print("División ", division)
-print("Suma: ", suma)
+
 redondeo = int(division)
 print("Redondeo division: ", redondeo)
