03fcb09069f0911642230ed37f9ea72166bf710d
serraguti <pacoserranox@gmail.com>
2025-02-07 19:23:08 +0100

    aaa

---
+++ python02variables.py
@@ -0,0 +1,13 @@
+print("Ejemplo variables")
+#ESTO ES UN COMENTARIO
+#COMENTAR: COMMAND + K + C
+# DESCOMENTAR: COMMAND + K + U
+# Las variables no se declaran con tipado
+# las variables las declaramos en minuscula
+numero = 99
+texto = "Variable de tipo texto (string)"
+print("El número es: ", numero)
+#print("Numero: " + numero)
+textonumero = "123AAAA"
+minumero = int(textonumero)
+#print("Variable " + texto + ", número: " + numero)
---
+++ python03mates.py
@@ -0,0 +1,13 @@
+print("Ejemplo mates variables")
+numero1 = 20
+numero2 = 3
+suma = numero1 + numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+resta = numero1 - numero2
+print("Resta ", resta)
+print(str(numero1) + " * " + str(numero2) + "=" + str(multiplicacion))
+print("División ", division)
+print("Suma: ", suma)
+redondeo = int(division)
+print("Redondeo division: ", redondeo)
