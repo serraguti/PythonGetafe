1c5fe1364d06c435f1091dc5556a6f0709ac1200
serraguti <pacoserranox@gmail.com>
2025-02-10 20:52:06 +0100

    lunesfin

---
+++ python12bucles.py
@@ -0,0 +1,9 @@
+print("Ejemplo bucles")
+print("WHILE")
+# NECESITAMOS UNA VARIABLE PARA LA CONDICION DEL BUCLE
+contador = 0
+while (contador <= 5):
+    # DEBEMOS INDICAR QUE SALDREMOS DEL BUCLE
+    print("Contador ", contador)
+    contador = contador + 1
+print("Fin de programa")
