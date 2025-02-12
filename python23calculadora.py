fa384335cf6bd56803536482f1942d385aacec0b
serraguti <pacoserranox@gmail.com>
2025-02-12 19:32:01 +0100

    version 2 calc

--- python23calculadora.py
+++ python23calculadora.py
@@ -9,6 +9,7 @@ def multiplicarNumeros(num1, num2):
     return num1 * num2
 
 def mostrarMenu():
+    print("0.- Salir")
     print("1.- Sumar")
     print("2.- Restar")
     print("3.- Multiplicar")
@@ -20,16 +21,29 @@ print("Introduzca numero 1")
 numero1 = int(input())
 print("Introduzca numero 2")
 numero2 = int(input())
-mostrarMenu()
-opcion = int(input())
-operacion = 0
-if (opcion == 1):
-    operacion = sumarNumeros(numero1, numero2)
-elif (opcion == 2):
-    operacion = restarNumeros(numero1, numero2)
-elif (opcion == 3):
-    operacion = multiplicarNumeros(numero1, numero2)
-else:
-    print("No ha seleccionado una opción correcta")
-print("Operación " + str(operacion))
+# ASIGNAMOS UN VALOR A OPCION PARA ENTRAR EN EL BUCLE
+opcion = 1
+# CREAMOS UN WHILE HASTA QUE EL USUARIO ESCRIBA 0
+while (opcion != 0):
+    mostrarMenu()
+    opcion = int(input())
+    operacion = 0
+    if (opcion == 1):
+        operacion = sumarNumeros(numero1, numero2)
+    elif (opcion == 2):
+        operacion = restarNumeros(numero1, numero2)
+    elif (opcion == 3):
+        operacion = multiplicarNumeros(numero1, numero2)
+    else:
+        print("No ha seleccionado una opción correcta")
+    print("Operación " + str(operacion))
 print("Fin de programa")
+
+
+
+
+
+
+
+
+
