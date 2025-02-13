f8db5a7f34c6cddecdd5ab63a03a4be3584e5706
serraguti <pacoserranox@gmail.com>
2025-02-13 20:21:38 +0100

    diccionarios

---
+++ python28diccionario.py
@@ -0,0 +1,19 @@
+print("Diccionarios Python")
+
+provincias = dict()
+provincias = {
+    924 : "Badajoz",
+    956:  "Cádiz",
+    958 : "Granada",
+    959 : "Huelva",
+    91 : "Madrid",
+    95 : "Málaga",
+    968 : "Murcia",
+    923 : "Salamanca",
+    95 : "Sevilla",
+    922 : "Sta. Cruz de Tenerife",
+    975 : "Soria",
+    96 : "Valencia",
+    976 : "Zaragoza"
+}
+print(provincias)
