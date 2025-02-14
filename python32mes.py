9f8ea3cc29683196766ac1a2a8c802327049ef29
serraguti <pacoserranox@gmail.com>
2025-02-14 20:27:01 +0100

    python mes 2

--- python32mes.py
+++ python32mes.py
@@ -8,3 +8,10 @@ enero.temperaturaMinima = -4
 media = enero.getTemperaturaMedia()
 print("Enero ", media)
 print(enero)
+febrero = Mes()
+febrero.nombre = "Febrero"
+febrero.temperaturaMaxima = 18
+febrero.temperaturaMinima = 4
+media = febrero.getTemperaturaMedia()
+print("febrero ", media)
+print(febrero)
